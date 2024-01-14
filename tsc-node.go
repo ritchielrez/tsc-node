aa958519e4fabfdeee640a6b94f61095bd3d7237
RitchielRez <ritchielrez@gmail.com>
2024-01-14 19:24:54 +0600

    feat: rename main.go to tsc-node.go
    `go install` compiles a go file and installs a binary
    to $GOROOT/bin which has the same name as the source file.

---
+++ tsc-node.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/cli/safeexec"
+)
+
+func runCommand(command string, args ...string) string {
+	commandBin, err := safeexec.LookPath(command)
+	if err != nil {
+		log.Fatalf("Error finding command %s, error %v\n", command, err)
+	}
+
+	cmd := exec.Command(commandBin, args...)
+	cmd_output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Error runnning command %s, error %v\n", command, err)
+	}
+
+	return strings.TrimSpace(string(cmd_output))
+}
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Fprintf(os.Stderr, "Usage: tsc-node <file-name>\n")
+		fmt.Fprintf(os.Stderr, "Error: file name not provided\n")
+	} else if len(args) > 1 {
+		fmt.Fprintf(os.Stderr, "Usage: tsc-node <file-name>\n")
+		fmt.Fprintf(os.Stderr, "Error: extra arguments provided\n")
+	} else if _, err := os.Stat(args[0]); err != nil && errors.Is(err, os.ErrNotExist) {
+		fmt.Fprintf(os.Stderr, "Error: file was not found\n")
+	} else {
+		tsFileName := args[0]
+		jsFileName := strings.TrimSuffix(tsFileName, ".ts") + ".js"
+
+		_, err := os.Stat(jsFileName)
+		if err != nil && errors.Is(err, os.ErrNotExist) {
+			_ = runCommand("tsc", tsFileName)
+		}
+
+		nodeCmdOut := runCommand("node", jsFileName)
+		fmt.Println(nodeCmdOut)
+	}
+}
